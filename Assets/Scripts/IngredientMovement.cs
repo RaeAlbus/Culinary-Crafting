6694ffc75aaf4f45268a53c9a2d7697782723d2c
Rae Albus <116688356+RaeAlbus@users.noreply.github.com>
2023-11-10 17:42:20 -0500

    Implemented Movable Ingredient Prefab

---
+++ Assets/Scripts/IngredientMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngredientMovement : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+        }
+    }
+}
