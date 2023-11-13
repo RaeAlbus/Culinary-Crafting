0185f7276dddb6b63d8421dff2ecc87bfa4cf66b
Rae Albus <116688356+RaeAlbus@users.noreply.github.com>
2023-11-13 13:01:52 -0500

    Implemented Sprite Rendering and Row change for Ingredients

--- Assets/Scripts/GenerateIngredients.cs
+++ Assets/Scripts/GenerateIngredients.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class GenerateIngredients : MonoBehaviour
 {
     private string[] ingredients;
+    public Sprite[] ingredientSprites;
     public GameObject IngredientPrefab;
     // Start is called before the first frame update
     void Start()
     {
+        float currRow = 0;
+        int numObjs = 1;
         float index = 0.0f;
-        ingredients = new string[]{"Water", "Sugar", "Flour", "Oil", "Eggs", "Salt", "Pepper", "Bread", "Turkey", "Cheese", "Lettuce", "Bowl", "Stove", "Pan", "Pot", "Knife"};
+        ingredients = new string[]{"Water", "Sugar", "Flour", "Oil", "Eggs", "Salt", "Pepper", "Bread", "Turkey", "Cheese", "Lettuce", "Bowl", "Stove", "Frying Pan", "Pot", "Knife"};
         foreach (string name in ingredients) {
-            GameObject spawnedPrefab = Instantiate(IngredientPrefab, transform.position + Vector3.right * index * 1.5f, transform.rotation);
+            GameObject spawnedPrefab = Instantiate(IngredientPrefab, new Vector3(transform.position.x + index * 2f, transform.position.y - currRow), transform.rotation);
             spawnedPrefab.name = name;
+
+            // Attaches sprite to GameObject
+            Sprite sprite = GetSpriteForIngredient(name);
+            SpriteRenderer spriteRenderer = spawnedPrefab.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = sprite;
+            float scaleFactor = 0.4f; // Adjust the scale factor as needed
+            spawnedPrefab.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+
             index += 1;
+            numObjs += 1;
+            if((numObjs != 1 && 11 % numObjs == 0) || (11 % numObjs == 0)){
+                currRow += 1.95f;
+                index = 0.0f;
+            }
         }
+    }
+
+    // Helper method to get the sprite for a specific ingredient
+    private Sprite GetSpriteForIngredient(string ingredientName)
+    {
+
+        string spriteName = ingredientName.Replace(" ", "");
+        return System.Array.Find(ingredientSprites, sprite => sprite.name == spriteName);
     }
 
     // Update is called once per frame
