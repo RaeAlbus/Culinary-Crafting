185d256061f296a19e778909405a183be5f1e96a
Rae Albus <116688356+RaeAlbus@users.noreply.github.com>
2023-11-13 14:37:20 -0500

    Added informative text and next step game logic

--- Assets/Scripts/LogicScript.cs
+++ Assets/Scripts/LogicScript.cs
@@ -7,19 +7,21 @@ using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
-    private bool correct;
+    public bool stepOneDone = false;
+    public bool stepTwoDone = false;
     public Text successText;
     private GameObject[] slots;
     private List<string> goal;
     private List<string> goal2;
     public GameObject IngredientPrefab;
+    public Sprite[] dishSprites;
 
     // Start is called before the first frame update
     void Start()
     {
         slots = GameObject.FindGameObjectsWithTag("Slot");
         goal = new List<string>{"Water", "Eggs", "Flour", "Sugar", "Bowl"};
-        goal2 = new List<string>{"Pancake Batter", "Pan", "Stove"};
+        goal2 = new List<string>{"Pancake Batter", "Frying Pan", "Stove"};
         goal.Sort();
         goal2.Sort();
     }
@@ -27,8 +29,16 @@ public class LogicScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(stepOneDone && stepTwoDone){
+            Invoke("NextLevel", 2.0f);
+        }
     }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
 
     public void updateText(string t) {
         successText.text = t;
@@ -44,18 +54,42 @@ public class LogicScript : MonoBehaviour
             }
         }
         ingNames.Sort();
-        if (goal.SequenceEqual(ingNames)) {
-            updateText("Success");
-            spawnIngredient("Pancake Batter");
-        } else if (goal2.SequenceEqual(ingNames)) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if(stepOneDone){
+            if (goal2.SequenceEqual(ingNames)) {
+                updateText("You made Pancakes!");
+                spawnIngredient("Pancake Batter");
+                stepTwoDone = true;
+            } else {
+                updateText("Not quite right...");
+            }
         } else {
-            updateText("Fail");
+            if (goal.SequenceEqual(ingNames)) {
+                updateText("You made Pancake Batter!");
+                spawnIngredient("Pancake Batter");
+                stepOneDone = true;
+            } else {
+                updateText("Not quite right...");
+            }
         }
     }
 
     public void spawnIngredient(string name) {
         GameObject spawnedPrefab = Instantiate(IngredientPrefab, transform.position, transform.rotation);
         spawnedPrefab.name = name;
+
+        // Attaches sprite to GameObject
+        Sprite sprite = GetSpriteForIngredient(name);
+        SpriteRenderer spriteRenderer = spawnedPrefab.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+        float scaleFactor = 0.4f; // Adjust the scale factor as needed
+        spawnedPrefab.transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+    }
+
+    // Helper method to get the sprite for a specific ingredient
+    private Sprite GetSpriteForIngredient(string ingredientName)
+    {
+        string spriteName = ingredientName.Replace(" ", "");
+        return System.Array.Find(dishSprites, sprite => sprite.name == spriteName);
     }
 }
